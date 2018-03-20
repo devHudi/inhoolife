d39c45884d83ab2257d483868489cea3c8517434
L0GIC <cdhnet98@gmail.com>
2018-03-20 21:35:47 +0900

    스피너 코멘트 표시방법을 state 사용으로 변경

--- client/src/containers/SpinnerContainer/SpinnerContainer.js
+++ client/src/containers/SpinnerContainer/SpinnerContainer.js
@@ -7,35 +7,44 @@ class SpinnerContainer extends Component {
     super(props)
     
     this.state = {
-      visible: false
+      visible: false,
+      comment: ""
     }
   }
 
   componentWillReceiveProps(nextProps) {
-    this.setState({
-      visible: nextProps.visible
-    })
+    if (nextProps.visible) {
+      const comments = [
+        "제가 골라드릴게요!",
+        "고르는 중이에요!",
+        "오늘 식사는 제가 책임지죠!",
+        "뭐가 나오던 무조건 먹기에요?"
+      ]
+      const rndIndex = Math.floor((Math.random() * (comments.length - 1)))
+      const comment = comments[rndIndex]
+
+      this.setState({
+        visible: nextProps.visible,
+        comment
+      })
+    } else {
+      this.setState({
+        visible: nextProps.visible,
+        comment: ""
+      })
+    }
   }
 
   render() {
     var visibleStyle = {
       display: this.state.visible ? "block" : "none"
     }
-
-    const comments = [
-      "제가 골라드릴게요!",
-      "고르는 중이에요!",
-      "오늘 식사는 제가 책임지죠!",
-      "뭐가 나오던 무조건 먹기에요?"
-    ]
-    const rndIndex = Math.floor((Math.random() * (comments.length - 1)))
-    const ment = comments[rndIndex]
     
     return (
       <div id="Spinner" style={ visibleStyle }>
         <div className="vertical"><div className="center">
         <DoubleBounce color="#FFFFFF" size={60}/>
-        <label> { ment } </label>
+        <label> { this.state.comment } </label>
         </div></div>
       </div>
     )
