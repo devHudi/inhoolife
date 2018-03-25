a4f19aad18461a13f1a8ea2146129b4cacf70b81
L0GIC <cdhnet98@gmail.com>
2018-03-26 01:31:11 +0900

    식당 개수 출력

--- server/app.js
+++ server/app.js
@@ -21,7 +21,8 @@ app.get("/list", (req, res) => {
 
   Restaurant.find({}, (err, doc) => {
     var text = "<meta charset='utf-8'>"
-    text = text + "추가 문의는 <a href='https://www.facebook.com/인후라이프-1930922233794951'> 인후라이프 페이스북</a> 으로 부탁드립니다.<br><br>"
+    text = text + "추가 문의는 <a href='https://www.facebook.com/인후라이프-1930922233794951'> 인후라이프 페이스북</a> 으로 부탁드립니다.<br>"
+    text = text + "총 " + doc.length + "개의 식당이 등록되어 있습니다. 실시간으로 추가중입니다.<br><br>"
 
     doc.map((restaurant) => {
       text = text + restaurant.name + "<br>"
