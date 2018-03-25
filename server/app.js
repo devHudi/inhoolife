db57a25a3ffa1e55b6a180634d012b0cb3688d84
L0GIC <cdhnet98@gmail.com>
2018-03-25 20:51:59 +0900

    식당 리스트에 페북주소 만듬

--- server/app.js
+++ server/app.js
@@ -21,6 +21,7 @@ app.get("/list", (req, res) => {
 
   Restaurant.find({}, (err, doc) => {
     var text = "<meta charset='utf-8'>"
+    text = text + "추가 문의는 <a href='https://www.facebook.com/인후라이프-1930922233794951'> 인후라이프 페이스북</a> 으로 부탁드립니다.<br><br>"
 
     doc.map((restaurant) => {
       text = text + restaurant.name + "<br>"
